560d8046da89c2dd90f2e2deadbc334e5897b36b
Mmahika <mahikaandgoel@gmail.com>
2025-02-04 15:33:44 +0530

    Added FastAPI app definition

--- app.py
+++ app.py
@@ -17,4 +17,11 @@ def test():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=5000)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
 
